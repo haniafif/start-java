b91c52c0e27fe8733eb5afde6b3ef7ba9ccdeb40
Hani <haniaboultaif@gmail.com>
2022-12-23 14:30:07 -0300

    Feature primeiro exercicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float numero = 0.5f;
+        float numero2 = 10.5f;
+        float soma = numero + numero2;
+        System.out.println("O valor do numero é:" + soma);
     }
 }
