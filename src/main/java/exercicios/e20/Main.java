3c614b68888905aacf337619f4fa908393812636
Hani <haniaboultaif@gmail.com>
2022-12-28 12:45:09 -0300

    Feature finish JAva

---
+++ src/main/java/exercicios/e20/Main.java
@@ -0,0 +1,12 @@
+package exercicios.e20;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+    public static void main(String[] args){
+        // Formatação de dados
+        SimpleDateFormat data =  new SimpleDateFormat("dd/MM/yyyy");
+    }
+}
